abf321b1d86298a1574a57755fdc7f138005cc4c
0x00evil <0x00evil@gmail.com>
2016-07-18 14:34:34 +0800

    Add set_new_listener_options/3 in Joker.Server

--- lib/joker_server.ex
+++ lib/joker_server.ex
@@ -5,7 +5,17 @@ defmodule JokerServer do
     GenServer.start_link(__MODULE__, :ok, [name: __MODULE__])
   end
 
+  def set_new_listener_options(ref, max_connections, protocol_options) do
+    GenServer.call(__MODULE__, {:set_new_listener_options, ref, max_connections, protocol_options})
+  end
+
   def init(:ok) do
     {:ok, %{}}
+  end
+
+  def handle_call({:set_new_listener_options, ref, max_connections, protocol_options}, _from, state) do
+    :ets.insert(:joker_server, {{:max_connections, ref}, max_connections})
+    :ets.insert(:joker_server, {{:protocol_options, ref}, protocol_options})
+    {:reply, :ok, state}
   end
 end
