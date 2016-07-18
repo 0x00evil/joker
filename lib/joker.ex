f8f8cb51526b0a05f3a7d5e039e6c5cc41db6427
0x00evil <0x00evil@gmail.com>
2016-07-18 14:32:55 +0800

    Add start_listener/6 in Joker.Supervisor

--- lib/joker.ex
+++ lib/joker.ex
@@ -2,4 +2,13 @@ defmodule Joker do
   def start(_type, _args) do
     Joker.Supervisor.start_link
   end
+
+  def start_listener(ref, acceptor_count, transport, transport_options, protocol, protocol_options) do
+    Supervisor.start_child(Joker.Supervisor, child_spec(ref, acceptor_count, transport, transport_options, protocol, protocol_options))
+  end
+
+  defp child_spec(ref, acceptor_count, transport, transport_options, protocol, protocol_options) do
+    import Supervisor.Spec
+    supervisor(Joker.ListenerSupervisor, [ref, acceptor_count, transport, transport_options, protocol, protocol_options], [id: {Joker.ListenerSupervisor, ref}])
+  end
 end
