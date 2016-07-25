645d159e16cb9f583df2d58340136087afa3e16e
0x00evil <0x00evil@gmail.com>
2016-07-25 15:59:14 +0800

    Make acceptor accept connection

--- lib/joker_acceptor.ex
+++ lib/joker_acceptor.ex
@@ -1,12 +1,21 @@
 defmodule Joker.Acceptor do
-  def start_link(ref, transport, transport_options) do
-    pid = spawn_link(__MODULE__, :loop, [ref, transport, transport_options])
+  def start_link(listen_socket, transport, connection_supervisor) do
+    pid = spawn_link(__MODULE__, :loop, [listen_socket, transport, connection_supervisor])
     {:ok, pid}
   end
 
-  def loop(ref, transport, transport_options) do
-    receive do
-      :stop -> :ok
+  def loop(listen_socket, transport, connection_supervisor) do
+    case transport.accept(listen_socket) do
+      {:ok, connection_socket} ->
+        case transport.controlling_process(connection_supervisor, connection_socket) do
+          :ok ->
+            Joker.ConnectionsSupervisor.start_protocol(connection_socket, connection_supervisor)
+          {:error, _} ->
+            transport.close(connection_socket)
+        end
+
+      _ -> IO.puts("socket error")
     end
+    loop(listen_socket, transport, connection_supervisor)
   end
 end
--- lib/joker_acceptors_supervisor.ex
+++ lib/joker_acceptors_supervisor.ex
@@ -2,12 +2,15 @@ defmodule Joker.AcceptorsSupervisor do
   use Supervisor
 
   def start_link(ref, acceptors_count, transport, transport_options) do
-    Supervisor.start_link(__MODULE__, [ref, acceptors_count, transport, transport_options])
+    Supervisor.start_link(__MODULE__, [ref, acceptors_count, transport, transport_options], [name: __MODULE__])
   end
 
   def init([ref, acceptors_count, transport, transport_options]) do
+    connection_supervisor = JokerServer.get_connection_supervisor(ref)
+    listen_socket = transport.listen(transport_options)
+
     children = for n <- 1..acceptors_count do
-       worker(Joker.Acceptor, [ref, transport, transport_options], [id: {:acceptor, self, n}])
+       worker(Joker.Acceptor, [listen_socket, transport, connection_supervisor], [id: {:acceptor, self, n}])
     end
 
     strategy = [strategy: :one_for_one]
