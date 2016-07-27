89c903aa53bfd53034a7c1016f1cbae835d14855
0x00evil <0x00evil@gmail.com>
2016-07-27 20:19:12 +0800

    Update app version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Joker.Mixfile do
 
   def project do
     [app: :joker,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.2",
      description: description,
      package: package,
